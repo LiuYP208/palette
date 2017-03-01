bcca7978189302899ed16aa0a65633f6b9849a6b
LiuYP <799327507@qq.com>
2017-03-01 14:45:22 +0800

    添加app部分

---
+++ ColorPallate.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lenovo on 2017/3/1.
+ */
+var ColorPallate = function () {
+    var self = this;
+    this.DIVID = "";
+    //调色板开始数值
+    var BeginNum = 0;
+    //调色板结束数值
+    var EndNum = 0;
+    this.init = function (m_DivID,m_Json) {
+        this.DIVID = m_DivID;
+        //解析 m_Json
+
+    }
+};
+
